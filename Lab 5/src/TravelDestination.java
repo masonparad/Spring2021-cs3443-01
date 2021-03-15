15a3080fb91443ce3be0233cfc6254c6c2a77cd3
Filiberto Rios <56402829+thousandSun@users.noreply.github.com>
2021-03-15 13:27:24 -0500

    Update TravelDestination.java
    added print

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -4,6 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
 		System.out.println("My name is Megan Garza. I Wish to travel to Colorado and Florida once Covid goes away!");
+		System.out.println("My name is Filiberto Rios. I Wish to travel to Japan once Covid goes away!");
 	}
 
 }
