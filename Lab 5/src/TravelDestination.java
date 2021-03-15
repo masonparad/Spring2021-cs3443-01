0cc996b8f6a2389d556476c313accf337ca716cc
Meg531 <72843833+Meg531@users.noreply.github.com>
2021-03-15 13:38:54 -0500

    Update TravelDestination.java
    I added a print statement with my name and travel destination for after covid.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,7 +3,7 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
-		
+		System.out.println("My name is Megan Garza. I wish to travel to Colorado and Florida pnce Covid is over.");
 	}
 
 }
