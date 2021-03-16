c66dfbb63b8ddb48066f348dd29236d5487deeaf
treicruz <80602243+treicruz@users.noreply.github.com>
2021-03-16 15:34:10 -0500

    Update TravelDestination.java
    re-added my information the pull request did not work for me. Tried 2 different ways.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -6,7 +6,8 @@ public class TravelDestination {
 		System.out.println("My name is Megan Garza. I wish to travel to Colorado and Florida pnce Covid is over.");
 		System.out.println("My name is Filiberto Rios. I Wish to travel to Japan once Covid goes away!");
 		System.out.println("My name is Nicholas Schultz. I wish to travel back to Germany once Covid goes away!");
-		
+		System.out.println("My name is Ernesto Cruz. I wish to travel to any beach once Covid goes away!");
+
 	}
 
 }
