133993fadf289d5092d6123aa664761070ea1cd2
Alex Bush <qdf438@my.utsa.edu>
2021-03-23 10:14:20 -0500

    updating my branch

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -8,11 +8,17 @@ public class TravelDestination {
 		System.out.println("My name is Nicholas Schultz. I wish to travel back to Germany once Covid goes away!");
 		System.out.println("My name is Ernesto Cruz. I wish to travel to any beach once Covid goes away!");
 		System.out.println("My name is Jasmine Beale. I wish to travel to New York once Covid goes away!");
-                System.out.println("My name is Isai Rivas. I wish to travel to Japan once Covid goes away!");
+		System.out.println("My name is Isai Rivas. I wish to travel to Japan once Covid goes away!");
 		System.out.println("My name is Gabriela Castellanos. I wish to travel to Thailand once Covid goes away!");	
 		System.out.println("My name is James Le. I wish to travel to Austin, TX once Covid goes away!");
 		System.out.println("My name is Aden Sawyer. I wish to travel to France once Covid goes away!");
 		System.out.println("My name is Daniel Rodriguez. I wish to travel to Japan once Covid goes away!");
+		System.out.println("My name is Steven Su. I wish to travel to visit friends once this Covid goes away!");
+		System.out.println("My name is Viswa Bhargavi. I wish to travel to Singapore once this Covid goes away!");
+		System.out.println("My name is Tyler Holstein. I wish to travel to Colorado once this Covid goes away!");
+		System.out.println("My name is Courtney Gaura. I wish to travel to New York once this Covid goes away!");
+		System.out.println("My name is Hailey Morris. I wish to travel to Korea once this Covid goes away!");
+		System.out.println("My name is Samuel Pilato. I wish to travel to Kansas once this Covid goes away!");
 		System.out.println("My name is Alex Bush. I wish to travel to Tokyo once this Covid goes away!");
 		
 	}
