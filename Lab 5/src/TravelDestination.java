9a240eb8a6311330c73b00d5ebee8f6cb2ac8d34
Alex Bush <qdf438@my.utsa.edu>
2021-03-23 10:25:45 -0500

    revisiing minor merge mistake

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -1,5 +1,3 @@
-qdf438-Lab5FirstCommitEdits
-
 public class TravelDestination {
 
 	public static void main(String[] args) {
@@ -26,30 +24,3 @@ public class TravelDestination {
 	}
 
 }
-=======
-
-public class TravelDestination {
-
-	public static void main(String[] args) {
-		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
-		System.out.println("My name is Megan Garza. I wish to travel to Colorado and Florida pnce Covid is over.");
-		System.out.println("My name is Filiberto Rios. I Wish to travel to Japan once Covid goes away!");
-		System.out.println("My name is Nicholas Schultz. I wish to travel back to Germany once Covid goes away!");
-		System.out.println("My name is Ernesto Cruz. I wish to travel to any beach once Covid goes away!");
-		System.out.println("My name is Jasmine Beale. I wish to travel to New York once Covid goes away!");
-		System.out.println("My name is Isai Rivas. I wish to travel to Japan once Covid goes away!");
-		System.out.println("My name is Gabriela Castellanos. I wish to travel to Thailand once Covid goes away!");	
-		System.out.println("My name is James Le. I wish to travel to Austin, TX once Covid goes away!");
-		System.out.println("My name is Aden Sawyer. I wish to travel to France once Covid goes away!");
-		System.out.println("My name is Daniel Rodriguez. I wish to travel to Japan once Covid goes away!");
-		System.out.println("My name is Steven Su. I wish to travel to visit friends once this Covid goes away!");
-		System.out.println("My name is Viswa Bhargavi. I wish to travel to Singapore once this Covid goes away!");
-		System.out.println("My name is Tyler Holstein. I wish to travel to Colorado once this Covid goes away!");
-		System.out.println("My name is Courtney Gaura. I wish to travel to New York once this Covid goes away!");
-		System.out.println("My name is Hailey Morris. I wish to travel to Korea once this Covid goes away!");
-		System.out.println("My name is Samuel Pilato. I wish to travel to Kansas once this Covid goes away!");
-		System.out.println("My name is Roland Sanchez. I wish to travel to Japan once covid goes away.");
-	}
-
-}
-main
