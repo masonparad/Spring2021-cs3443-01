5bfc8b571fd7c8eecc79cc2dfdb51276c3f2b94c
JeffreyFlowers22 <80611672+JeffreyFlowers22@users.noreply.github.com>
2021-03-23 13:47:21 -0500

    Update TravelDestination.java
    Added a new print statement for where Jeffrey would like to travel to.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -26,6 +26,7 @@ public class TravelDestination {
 		System.out.println("My name is Marianno Reynoso. I wish to travel to Italy once this Covid goes away!");
 		System.out.println("My name is Lisette Isais. I wish to travel to Oregon once this Covid goes away!");
 		System.out.println("My name is Duy Le. I wish to travel to Japan once this Covid goes away!");
+		System.out.println("My name is Jeffrey Flowers. I wish to travel to California once this Covid goes away!");
 
 	}
 
