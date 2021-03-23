ea279c1a1a1c94f8bf125272aff2066a6641c4c3
Alex Bush <qdf438@my.utsa.edu>
2021-03-23 10:08:58 -0500

    reversing my edit so it can be redone as a pull request.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -13,7 +13,6 @@ public class TravelDestination {
 		System.out.println("My name is James Le. I wish to travel to Austin, TX once Covid goes away!");
 		System.out.println("My name is Aden Sawyer. I wish to travel to France once Covid goes away!");
 		System.out.println("My name is Daniel Rodriguez. I wish to travel to Japan once Covid goes away!");
-		System.out.println("My name is Alexander Bush. I wish to travel to Tokyo once this Covid goes away!");
 		System.out.println("My name is Steven Su. I wish to travel to visit friends once this Covid goes away!");
 		System.out.println("My name is Viswa Bhargavi. I wish to travel to Singapore once this Covid goes away!");
 		System.out.println("My name is Tyler Holstein. I wish to travel to Colorado once this Covid goes away!");
