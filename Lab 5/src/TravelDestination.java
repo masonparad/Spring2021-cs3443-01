3b4055858b3126f0a1036661d491a89333693298
Mark Zimmerschied <eyc259@my.utsa.edu>
2021-03-23 10:08:28 -0500

    added my line to TravelDestinatnio.java

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -20,6 +20,7 @@ public class TravelDestination {
 		System.out.println("My name is Courtney Gaura. I wish to travel to New York once this Covid goes away!");
 		System.out.println("My name is Hailey Morris. I wish to travel to Korea once this Covid goes away!");
 		System.out.println("My name is Samuel Pilato. I wish to travel to Kansas once this Covid goes away!");
-	}
+		System.out.println("My name is Mark Zimmerschied. I wish to travel to Japan once this Covid goes away!");
+	}	
 
 }
