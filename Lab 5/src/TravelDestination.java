a33c23b66570be71c1b3f03a4f3253c6acc8c0a1
Marianno-rey <72538175+Marianno-rey@users.noreply.github.com>
2021-03-23 12:01:44 -0500

    Update TravelDestination.java
    My first commit to the main branch!

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -23,6 +23,8 @@ public class TravelDestination {
 		System.out.println("My name is Alexander Bush. I wish to travel to Tokyo once this Covid goes away!");
 		System.out.println("My name is Yzan Qaryouti. I wish to travel to Spain once this Covid goes away!");
 		System.out.println("My name is Pablo Marroquin. I wish to travel to Scotland once this Covid goes away!");
+		System.out.println("My name is Marianno Reynoso. I wish to travel to Italy once this Covid goes away!");
+
 	}
 
 }
